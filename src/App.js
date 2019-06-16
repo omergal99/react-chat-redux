c791e612dd269cee7470fe9337fb060e23ad1901
omergal99 <omergal99gm@gmail.com>
2019-06-16 03:11:33 +0300

    clean

--- src/App.js
+++ src/App.js
@@ -1,19 +1,15 @@
-// import React from 'react';
 import React, { Component } from 'react';
 import './assets/css/App.scss';
-// import AppStore from './store/AppStore';
 
 import Chat from './pages/Chat';
 
 class App extends Component {
   componentDidMount() {
-    // AppStore.userStore.loadUser()
   }
 
   render() {
     return (
       <div className="App">
-        {/* <Chat store={AppStore} /> */}
         <Chat />
       </div>
     );
